2816a4b7fccfdc51a0401a8bea14bef591e3a9a7
Juan C Ruiz <juanalgostudio@gmail.com>
2021-10-03 13:43:06 -0500

    TenSalesApp in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import logo from './components/TenSalesAppLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>TenSalesApp</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a class="App-link" href={"./login.html"}>Clic para continuar</a>
+        <div class="footer">
+        <img class="logo10js" src="./images/logoTenJS.png" alt="Logo TenJS" />
+        <p id="footer_p" ALIGN="middle">© 2021 TenJS. ALL RIGHTS RESERVED.</p>
+        </div>
+
       </header>
+
     </div>
   );
 }
